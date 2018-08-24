99faca3337848b71c9645d2644a4d932fb34c4c8
Amil Masic <amil.masic@gmail.com>
2018-08-24 22:21:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
+  else if int % 3 == 0
+    "Fizz"
   end
-else if int % 3 == 0
-  "Fizz"
-end
+  end
+
 # else if int % 5 == 0
 #   "Buzz"
 end
